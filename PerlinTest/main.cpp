6bd6a607a3109b47b4b8b23958afc7b207b0605f
Kieren Pearson <kierenpearson9@gmail.com>
2015-12-08 01:22:10 +1100

    Removed reduntant testing code

--- PerlinTest/main.cpp
+++ PerlinTest/main.cpp
@@ -7,45 +7,26 @@
 
 using namespace::MathLib;
 
-TileEngine MyEngine(Vector2(20, 20));
-
+const unsigned int Octives = 4;
+const float Frequency = 3;
 const float Magnitude = 6;
+const float Persistance = 1 / sqrt(2);
+const int Seed = 6;
+
+const Vector2 WorldSize(20, 20);
+
+TileEngine MyEngine(WorldSize);
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1500, 1000), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
 
 	MyEngine.SetTileSize(Vector2(64, 64));
 	sf::Texture MyTexture;
 	MyTexture.loadFromFile("TileSet.png");
 	MyEngine.SetTexture(MyTexture);
-	MyEngine.CheckSolid(1000, 1000);
 
-	Perlin *perlin = new Perlin(4, 3, Magnitude / 2, 0.7, 7);
-	sf::Texture PerlinTexture;
-	sf::Uint8* PerlinPixles = new sf::Uint8[500 * 500 * 4];
-
-	int f = 0.1;
-
-	for (int i = 0; i < 500; i++)
-	{
-		for (int j = 0; j < 500; j++)
-		{
-			float value = perlin->Get(static_cast<float>(i) / 500, static_cast<float>(j) / 500);
-			PerlinPixles[(i * 500 + j) * 4] = (value + Magnitude / 2) * (255 / 6);
-			PerlinPixles[(i * 500 + j) * 4 + 1] = (value + Magnitude / 2) * (255 / 6);
-			PerlinPixles[(i * 500 + j) * 4 + 2] = (value + Magnitude / 2) * (255 / 6);
-			PerlinPixles[(i * 500 + j) * 4 + 3] = 255;
-		}
-	}
-
-	PerlinTexture.create(500, 500);
-	PerlinTexture.update(PerlinPixles);
-	sf::Sprite MySprite;
-	MySprite.setPosition(640, 0);
-	MySprite.setTexture(PerlinTexture);
-
-	MyEngine.GenerateFromPerlin(4, 3, Magnitude, 0.5, 7);
+	MyEngine.GenerateFromPerlin(Octives, Frequency, Magnitude, Persistance, Seed);
 
 	while (window.isOpen())
 	{
@@ -58,7 +39,6 @@ int main()
 
 		window.clear();
 		MyEngine.Render(&window);
-		//window.draw(MySprite);
 		window.display();
 	}
 
